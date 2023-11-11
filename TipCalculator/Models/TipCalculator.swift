9ffdcb4e249634dd4d710baf7df7250088aca91f
Iader E. García Gómez <iadergg@gmail.com>
2023-11-11 18:22:42 -0500

    Add tests target
    - feat: add test target
    - feat: add TipCalculator logic

---
+++ TipCalculator/Models/TipCalculator.swift
@@ -0,0 +1,15 @@
+//
+//  TipCalculator.swift
+//  TipCalculator
+//
+//  Created by Iader Emir Garcia Gomez on 11/11/23.
+//
+
+import Foundation
+
+class TipCalculator {
+    
+    func calculate(total: Double, tipPercentage: Double) -> Double {
+        return total * tipPercentage
+    }
+}
---
+++ TipCalculatorTests/TipCalculatorTests.swift
@@ -0,0 +1,27 @@
+//
+//  TipCalculatorTests.swift
+//  TipCalculatorTests
+//
+//  Created by Iader Emir Garcia Gomez on 11/11/23.
+//
+
+import XCTest
+
+final class when_calculating_tip_based_on_total_amount: XCTestCase {
+    
+    func test_should_calculate_tip_sucessfully() {
+        let tipCalculator = TipCalculator()
+        let tip = tipCalculator.calculate(total: 100, tipPercentage: 0.1)
+        XCTAssertEqual(10, tip)
+    }
+}
+
+final class when_calculating_tip_based_on_negative_total_amount: XCTestCase {
+    
+    func test_should_throw_invalid_input_exception() {
+        let tipCalculator = TipCalculator()
+        XCTAssertThrowsError(try tipCalculator.calculate(total: -100, tipPercentage: 0.1), "") { error in
+            XCTAssertEqual(error as! TipCalculatorError, TipCalculatorError.invalidInput)
+        }
+    }
+}
